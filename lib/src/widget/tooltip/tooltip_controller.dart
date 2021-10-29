0f1066b6155b577039a54aee230b70f6818c13b1
Andrii Antonov <pro100svitlo@gmail.com>
2021-10-29 17:22:04 +0200

    Export to xayn_design tooltips from the xayn_search app
    - port all portable classes
    - add svg and provider dependency to the xayn_design

---
+++ lib/src/widget/tooltip/tooltip_controller.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/widgets.dart';
+import 'package:xayn_design/xayn_design.dart';
+
+class TooltipController extends ChangeNotifier {
+  MessageFactory _customFactory = const {};
+  TooltipKey? _activeKey;
+  List<dynamic>? _activeParameters;
+
+  /// The key for the message which is actively being displayed.
+  /// The key should is tied to a handler which returns an implementation of [TooltipMessage].
+  TooltipKey? get activeKey => _activeKey;
+
+  /// A List of optional parameters which can be passed to the [TooltipMessage]
+  /// which is created from the corresponding [activeKey].
+  List<dynamic>? get activeParameters => _activeParameters;
+
+  MessageFactory get customFactory => _customFactory;
+
+  TooltipController();
+
+  String keyToLabel(TooltipKey key) => _customFactory[key]!.label;
+
+  /// Registers a new [key] with a corresponding [builder].
+  void register({required TooltipKey key, required TooltipParams params}) {
+    _customFactory = Map<TooltipKey, TooltipParams>.from(_customFactory);
+
+    _customFactory[key] = params;
+  }
+
+  /// Displays a tooltip for the requested [key].
+  /// Use [parameters] to provide optional information, which can be used
+  /// when creating a corresponding [TooltipMessage].
+  void show(TooltipKey key, [List<dynamic>? parameters]) {
+    if (key != _activeKey) {
+      _activeKey = key;
+      _activeParameters = parameters;
+
+      notifyListeners();
+    }
+  }
+
+  /// Hides the active tooltip.
+  void hide() {
+    if (_activeKey != null) {
+      _activeKey = null;
+      _activeParameters = null;
+
+      notifyListeners();
+    }
+  }
+}
--- lib/xayn_design.dart
+++ lib/xayn_design.dart
@@ -12,3 +12,17 @@ export 'src/linden/xsizes.dart' show XSizes, Experiences;
 export 'src/linden/xcolors.dart' show XColors;
 export 'src/linden/xstyles.dart' show XStyles;
 export 'src/utils/safe_platform.dart' show SafePlatform;
+
+export 'src/widget/build_observer.dart';
+export 'src/widget/tooltip/messages/textual_notification.dart';
+export 'src/widget/tooltip/messages/tooltip_message.dart';
+export 'src/widget/tooltip/messages/tooltip_message_key.dart';
+export 'src/widget/tooltip/application_tooltip_provider.dart';
+export 'src/widget/tooltip/tooltip.dart';
+export 'src/widget/tooltip/tooltip_context_provider_state_mixin.dart';
+export 'src/widget/tooltip/tooltip_controller.dart';
+export 'src/widget/tooltip/tooltip_options.dart';
+export 'src/widget/tooltip/tooltip_state_mixin.dart';
+export 'src/widget/tooltip/tooltip_stateless_mixin.dart';
+
+export 'package:flutter_svg/flutter_svg.dart';
