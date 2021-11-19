56d50e024e4f650d34360971d5fb06d80067dbd2
Andrii Antonov <pro100svitlo@gmail.com>
2021-11-19 13:13:40 +0100

    Add minor refactoring to the settings_selectable widget
    - split onto smaller logical widgets
    - that was needed to simplify testing part

--- lib/src/widget/settings/settings.dart
+++ lib/src/widget/settings/settings.dart
@@ -7,5 +7,5 @@ export 'data/settings_tile_data.dart';
 export 'widget/settings_card.dart';
 export 'widget/settings_group.dart';
 export 'widget/settings_section.dart';
-export 'widget/settings_selectable.dart';
+export 'widget/settings_selectable.dart' show SettingsSelectable;
 export 'widget/settings_tile.dart';
