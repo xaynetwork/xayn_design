bc12ead5d61f8d4198e5ff02b34b1ef37a8024e3
Carmine Toriello <carmine.toriello@xayn.com>
2021-11-19 11:28:27 +0100

    Added comments

--- lib/src/widget/dots_indicator/dots_decorator.dart
+++ lib/src/widget/dots_indicator/dots_decorator.dart
@@ -5,13 +5,41 @@ const kDefaultShape = BoxShape.circle;
 const kDefaultSize = Size.square(12.0);
 const kDefaultBorderWidth = 2.0;
 
+/// Used for adding decoration to the [DotsIndicator] widget
 class DotsDecorator {
+  /// Inactive dot color
+  ///
+  /// @Default `Colors.transparent`
   final Color color;
+
+  /// Active dot color
+  ///
+  /// @Default `Colors.white`
   final Color activeColor;
+
+  /// Dot border color
+  ///
+  /// @Default `Colors.white`
   final Color borderColor;
+
+  /// Dot border width
+  ///
+  /// @Default `2.0`
   final double borderWidth;
+
+  /// Space between dots
+  ///
+  /// @Default `EdgeInsets.all(4.0)`
   final EdgeInsets spacing;
+
+  /// Dot shape
+  ///
+  /// @Default `BoxShape.circle`
   final BoxShape shape;
+
+  /// Dot size
+  ///
+  /// @Default `Size.square(12.0)`
   final Size size;
 
   const DotsDecorator({
