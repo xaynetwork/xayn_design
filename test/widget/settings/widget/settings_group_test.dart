6c2dd898be88c205ea0bdbc20ff3b80598007a7c
Andrii Antonov <pro100svitlo@gmail.com>
2021-11-19 11:43:01 +0100

    Add settings_group_test.dart

---
+++ test/widget/settings/widget/settings_group_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xayn_design/xayn_design.dart';
+
+import '../../../utils/widget_test_utils.dart';
+
+void main() {
+  const groupTitle = 'group title';
+  const tile0 = SettingsTileData(title: 'tile title 0');
+  const tile1 = SettingsTileData(title: 'tile title 1');
+  const tile2 = SettingsTileData(title: 'tile title 2');
+
+  testWidgets(
+    'GIVEN data with 2 items without title WHEN render object THEN find 2 tile widget',
+    (final WidgetTester tester) async {
+      final data = SettingsGroupData(items: const [tile0, tile1]);
+
+      await tester.pumpAppWrapped(SettingsGroup(data: data));
+
+      final titleFinder = find.text(groupTitle);
+
+      expect(titleFinder, findsNothing);
+      expect(find.byType(SettingsTile), findsNWidgets(2));
+      // 2 tile titles
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.byType(SettingsGroup), findsOneWidget);
+    },
+  );
+  testWidgets(
+    'GIVEN data with 2 items with title WHEN render object THEN find 2 tile widget and group title',
+    (final WidgetTester tester) async {
+      final data = SettingsGroupData(
+        items: const [tile0, tile1],
+        title: groupTitle,
+      );
+
+      await tester.pumpAppWrapped(SettingsGroup(data: data));
+
+      final titleFinder = find.text(groupTitle);
+
+      expect(titleFinder, findsOneWidget);
+      expect(find.byType(SettingsTile), findsNWidgets(2));
+      // 2 tile titles and one group title
+      expect(find.byType(Text), findsNWidgets(3));
+      expect(find.byType(SettingsGroup), findsOneWidget);
+    },
+  );
+  testWidgets(
+    'GIVEN data with 3 items WHEN render object THEN verify correct items order',
+    (final WidgetTester tester) async {
+      final data = SettingsGroupData(items: const [tile2, tile1, tile0]);
+
+      await tester.pumpAppWrapped(SettingsGroup(data: data));
+
+      final tileFinder = find.byType(SettingsTile);
+
+      // verify correct order
+      final tiles = tileFinder
+          .evaluate()
+          .map((e) => (e.widget as SettingsTile).data)
+          .toList();
+      expect(tiles, data.items);
+    },
+  );
+}
