e6a38ed3ed9806a968aa77032b97bb96182aabe9
Andrii Antonov <pro100svitlo@gmail.com>
2021-12-01 16:32:22 +0100

    Improve exports for the dots_indicator

--- lib/xayn_design.dart
+++ lib/xayn_design.dart
@@ -19,7 +19,6 @@ export 'src/utils/design_testing_utils.dart';
 export 'src/widget/build_observer.dart';
 export 'src/widget/button/app_raised_button.dart';
 export 'src/widget/card/app_card.dart';
-export 'src/widget/dots_indicator/dots_decorator.dart';
 export 'src/widget/dots_indicator/dots_indicator.dart';
 export 'src/widget/nav_bar/nav_bar.dart';
 export 'src/widget/settings/settings.dart';
