b969e3b49adb82201fe6da8af191eea5f65e30b5
Andrii Antonov <pro100svitlo@gmail.com>
2021-12-06 11:14:57 +0100

    Fix xayn_design export for the test related code

---
+++ lib/xayn_design_test.dart
@@ -0,0 +1,7 @@
+/// Flutter widgets and design objects for Xayn Design library
+/// exposing [Linden] design object.
+///
+/// To use, import `package:xayn_design/xayn_design.dart`.
+library xayn_design;
+
+export 'src/utils/design_testing_utils.dart';
