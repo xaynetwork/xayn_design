4d1f6043391f162bcddd3d74823ad8f6c5a8becd
Andrii Antonov <pro100svitlo@gmail.com>
2021-12-10 11:52:51 +0100

    Add tests for nav_bar_container.dart with utils

---
+++ test/widget/nav_bar/widget/nav_bar_container_test.dart
@@ -0,0 +1,359 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:xayn_design/src/widget/nav_bar/data/config_pair.dart';
+import 'package:xayn_design/src/widget/nav_bar/widget/nav_bar.dart';
+import 'package:xayn_design/src/widget/nav_bar/widget/nav_bar_container.dart';
+import 'package:xayn_design/xayn_design.dart';
+import 'package:xayn_design/xayn_design_test.dart';
+
+part 'nav_bar_container_test.utils.dart';
+
+void main() {
+  testWidgets(
+    'GIVEN NavBatContainer THEN verify widget tree is as expected',
+    (final WidgetTester tester) async {
+      await tester.pumpLindenApp(buildWidget());
+
+      final containerFinder = find.byType(NavBarContainer);
+      final inheritedFinder = find.byType(InheritedNavBarContainer);
+      final inheritedNavBarContainer =
+          inheritedFinder.evaluate().first.widget as InheritedNavBarContainer;
+
+      expect(containerFinder, findsOneWidget);
+      expect(inheritedFinder, findsOneWidget);
+      expect(inheritedNavBarContainer, isA<InheritedWidget>());
+      expect(getState(), isA<NavBarController>());
+    },
+  );
+
+  testWidgets(
+    'GIVEN widget with config WHEN resetNavBar called THEN grab configs every time',
+    (final WidgetTester tester) async {
+      const expectedCallCounter = 3;
+
+      final listOfConfigs = <ConfigPair>{};
+
+      for (int i = 0; i < expectedCallCounter; i++) {
+        final config = creteConfig(numberOfItems: i + 1);
+        await tester.pumpLindenApp(buildWidget(
+          child: _StatelessConfigWidget(() => config),
+        ));
+
+        resetNavBar();
+
+        await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+        await tester.pumpAndSettle();
+        listOfConfigs.add(getState().configPair!);
+      }
+
+      expect(listOfConfigs.length, equals(expectedCallCounter));
+    },
+  );
+
+  testWidgets(
+    'GIVEN widget with config WHEN update bar called THEN configs will be taken only for the first time',
+    (final WidgetTester tester) async {
+      final listOfConfigs = <ConfigPair>{};
+
+      for (int i = 0; i < 5; i++) {
+        final config = creteConfig(numberOfItems: i + 1);
+        await tester.pumpLindenApp(buildWidget(
+          child: _StatelessConfigWidget(() => config),
+        ));
+        await tester.pumpAndSettle();
+
+        NavBarContainer.updateNavBar(getContext());
+
+        await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+        await tester.pumpAndSettle();
+        listOfConfigs.add(getState().configPair!);
+      }
+
+      expect(listOfConfigs.length, equals(1));
+    },
+  );
+
+  testWidgets(
+    'GIVEN multiple calls in a row of the updateNavBar WHEN create a widget THEN debounce apply and will be took only last config',
+    (final WidgetTester tester) async {
+      final listOfConfigs = <ConfigPair?>{};
+
+      const steps = 5;
+      late NavBarConfig latestConfig;
+      for (int i = 0; i < steps; i++) {
+        latestConfig = creteConfig(numberOfItems: i + 1);
+        await tester.pumpLindenApp(buildWidget(
+          child: _StatelessConfigWidget(() => latestConfig),
+        ));
+        NavBarContainer.updateNavBar(getContext());
+        final configPair = getState().configPair;
+        if (configPair != null) {
+          listOfConfigs.add(configPair);
+        }
+        expect(listOfConfigs, isEmpty);
+      }
+
+      await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+      listOfConfigs.add(getState().configPair);
+      await tester.pumpAndSettle();
+
+      expect(listOfConfigs.length, equals(1));
+      expect(
+        listOfConfigs.first!.configMixins.first.navBarConfig,
+        latestConfig,
+      );
+    },
+  );
+
+  testWidgets(
+    'WHEN resetBar called in state THEN configPair is dropped to null',
+    (final WidgetTester tester) async {
+      await tester.pumpLindenApp(buildWidget(
+        child: _StatelessConfigWidget(() => _backBtnConfig),
+      ));
+      final state = getState();
+
+      resetNavBar();
+      await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+
+      // verify that it is NOT null after reset and debounce
+      expect(state.configPair, isNotNull);
+
+      resetNavBar();
+      // verify that it is null after reset before debounce done
+      expect(state.configPair, isNull);
+
+      await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+    },
+  );
+
+  testWidgets(
+    'WHEN dispose called THEN nothing happened after reset or update called (streams closed)',
+    (final WidgetTester tester) async {
+      await tester.pumpLindenApp(buildWidget(
+        child: _StatelessConfigWidget(() => _backBtnConfig),
+      ));
+      final state = getState();
+
+      expect(state.resetStream.isClosed, isFalse);
+      expect(state.updateStream.isClosed, isFalse);
+
+      // What we are doing here:
+      // we need to remove NavBarContainer from the widgetTree
+      // to force NavBarContainerStata.dispose() to be called.
+      // And the easiest way to do it - replace it with another widget :)
+      await tester.pumpLindenApp(const Center());
+
+      expect(state.resetStream.isClosed, isTrue);
+      expect(state.updateStream.isClosed, isTrue);
+    },
+  );
+
+  group(
+    'config changed in the NavBar',
+    () {
+      testWidgets(
+        'WHEN NavBarContainer.updateNavBar called THEN config in NavBar changed',
+        (final WidgetTester tester) async {
+          await tester.pumpLindenApp(buildWidget(
+            child: _StatelessConfigWidget(() => _backBtnConfig),
+          ));
+
+          final stateNavBar = getStateNavBar();
+          expect(stateNavBar.config, isNull);
+
+          NavBarContainer.updateNavBar(getContext());
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+
+          expect(stateNavBar.config, equals(_backBtnConfig));
+        },
+      );
+      testWidgets(
+        'WHEN NavBarContainer.resetNavBar called THEN config in NavBar changed',
+        (final WidgetTester tester) async {
+          var firstTimeCall = true;
+          await tester.pumpLindenApp(buildWidget(
+            child: _StatelessConfigWidget(
+              () => firstTimeCall ? _backBtnConfig : _singleItemConfig,
+            ),
+          ));
+
+          final stateNavBar = getStateNavBar();
+          expect(stateNavBar.config, isNull);
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, equals(_backBtnConfig));
+          firstTimeCall = false;
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, equals(_singleItemConfig));
+
+          // wait until NavBar update finished
+          await tester.pumpAndSettle();
+        },
+      );
+      testWidgets(
+        'GIVEN configPair without mixin WHEN update nav THEN config in NavBar changed to null',
+        (final WidgetTester tester) async {
+          await tester.pumpLindenApp(buildWidget(
+            child: _StatelessConfigWidget(
+              () => _backBtnConfig,
+            ),
+          ));
+
+          final stateNavBar = getStateNavBar();
+          expect(stateNavBar.config, isNull);
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, equals(_backBtnConfig));
+          // we need code above to make sure it is not null
+
+          // now we pushing screen without NavBarConfigMixin
+          await tester.pumpLindenApp(buildWidget());
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, isNull);
+
+          // wait until NavBar update finished
+          await tester.pumpAndSettle();
+        },
+      );
+      testWidgets(
+        'GIVEN configPair with configType.ignored WHEN update nav THEN config in NavBar is still null',
+        (final WidgetTester tester) async {
+          var firstTimeCall = true;
+          await tester.pumpLindenApp(buildWidget(
+            child: _StatelessConfigWidget(
+              () => firstTimeCall ? _backBtnConfig : _ignoredConfig,
+            ),
+          ));
+
+          final stateNavBar = getStateNavBar();
+          expect(stateNavBar.config, isNull);
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, equals(_backBtnConfig));
+          // we need code above to make sure the config was not null
+
+          // now we will return an ignored config instead of backBtn
+          firstTimeCall = false;
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(stateNavBar.config, isNull);
+          // we want to verify that ignored `config` was ignored and new config is null
+
+          // wait until NavBar update finished
+          await tester.pumpAndSettle();
+        },
+      );
+      testWidgets(
+        'GIVEN configMixins list with 2 items WHEN _updateBar method called THEN forEach happened in reversed way and latest item in the list will apply',
+        // the latest widget in the widget tree with the NavBarConfigMixin will be applied
+        (final WidgetTester tester) async {
+          final widget = _StatelessConfigWidget(
+            () => _singleItemConfig,
+            child: _StatelessConfigWidget(() => _backBtnConfig),
+          );
+          await tester.pumpLindenApp(buildWidget(child: widget));
+
+          final stateNavBar = getStateNavBar();
+
+          expect(stateNavBar.config, isNull);
+
+          resetNavBar();
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+
+          expect(stateNavBar.config, _backBtnConfig);
+        },
+      );
+    },
+  );
+
+  group(
+    'static methods',
+    () {
+      Future<BuildContext> buildTreeWithoutNavBarContainer(
+        WidgetTester tester,
+      ) async {
+        await tester.pumpLindenApp(const Center());
+        return find.byType(Center).evaluate().first;
+      }
+
+      testWidgets(
+        'GIVEN widget tree without NavBarContainer WHEN calling reset and update NavBar THEN throw exception',
+        (final WidgetTester tester) async {
+          final context = await buildTreeWithoutNavBarContainer(tester);
+
+          expect(
+            () => resetNavBar(context: context),
+            throwsA(isA<NavBarContainerNotFoundException>()),
+          );
+          expect(
+            () => resetNavBar(context: context, goingBack: true),
+            throwsA(isA<NavBarContainerNotFoundException>()),
+          );
+          expect(
+            () => updateNavBar(context: context),
+            throwsA(isA<NavBarContainerNotFoundException>()),
+          );
+        },
+      );
+      testWidgets(
+        'GIVEN staticCallsEnabled == false WHEN calling reset and update NavBar THEN nothing happened',
+        (final WidgetTester tester) async {
+          final context = await buildTreeWithoutNavBarContainer(tester);
+
+          NavBarContainer.staticCallsEnabled = false;
+
+          resetNavBar(context: context);
+          resetNavBar(context: context, goingBack: true);
+          updateNavBar(context: context);
+
+          NavBarContainer.staticCallsEnabled = true;
+        },
+      );
+    },
+  );
+
+  group(
+    'reset method check for stateless and stateful',
+    () {
+      testWidgets(
+        'GIVEN stateless WHEN resetNavBar THEN verify config is not null',
+        (final WidgetTester tester) async {
+          final configWidget = _StatelessConfigWidget(() => _backBtnConfig);
+          await tester.pumpLindenApp(buildWidget(child: configWidget));
+
+          final state = getState();
+          expect(state.configPair, isNull);
+
+          resetNavBar();
+
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(state.configPair, isNotNull);
+        },
+      );
+
+      testWidgets(
+        'GIVEN stateful WHEN resetNavBar THEN verify config is not null',
+        (final WidgetTester tester) async {
+          final configWidget = _StatefulConfigWidget(() => _backBtnConfig);
+          await tester.pumpLindenApp(buildWidget(child: configWidget));
+
+          final state = getState();
+          expect(state.configPair, isNull);
+
+          resetNavBar();
+
+          await tester.pumpAndSettle(updateNabBarDebounceTimeout);
+          expect(state.configPair, isNotNull);
+        },
+      );
+    },
+  );
+}
---
+++ test/widget/nav_bar/widget/nav_bar_container_test.utils.dart
@@ -0,0 +1,97 @@
+part of 'nav_bar_container_test.dart';
+
+const keyStack = Key('keyStack');
+
+final _backBtnConfig = NavBarConfig.backBtn(
+  NavBarItemBackButton(onPressed: () {}, key: const Key('back btn')),
+);
+
+final _ignoredConfig = NavBarConfig.ignored();
+
+final _singleItemConfig = creteConfig();
+
+BuildContext getContext() => find.byKey(keyStack).evaluate().first;
+
+NavBarContainerState getState() =>
+    (find.byType(NavBarContainer).evaluate().first as StatefulElement).state
+        as NavBarContainerState;
+
+NavBarState getStateNavBar() =>
+    (find.byType(NavBar).evaluate().first as StatefulElement).state
+        as NavBarState;
+
+NavBarConfig creteConfig({int numberOfItems = 1}) {
+  assert(numberOfItems >= 1, 'There should be at least 1 item');
+  final items = <NavBarItem>[];
+
+  for (int i = 0; i < numberOfItems; i++) {
+    final item = NavBarItemIconButton(
+      svgIconPath: 'packages/xayn_design/assets/icons/arrow_left.svg',
+      isHighlighted: i == 0,
+      onPressed: () {},
+      key: Key('item-$i'),
+    );
+    items.add(item);
+  }
+  return NavBarConfig(items);
+}
+
+void resetNavBar({BuildContext? context, goingBack = false}) =>
+    NavBarContainer.resetNavBar(context ?? getContext(), goingBack: goingBack);
+
+void updateNavBar({BuildContext? context}) =>
+    NavBarContainer.updateNavBar(context ?? getContext());
+
+NavBarContainer buildWidget({
+  Widget? child,
+}) =>
+    NavBarContainer(
+        child: Stack(
+      key: keyStack,
+      children: [
+        child ?? const SizedBox(),
+        const NavBar(),
+      ],
+    ));
+
+typedef GetConfigCallback = NavBarConfig Function();
+
+class _StatelessConfigWidget extends StatelessWidget with NavBarConfigMixin {
+  final Widget? child;
+  final GetConfigCallback getConfigCallback;
+
+  const _StatelessConfigWidget(
+    this.getConfigCallback, {
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => child ?? const Center();
+
+  @override
+  NavBarConfig get navBarConfig => getConfigCallback();
+}
+
+class _StatefulConfigWidget extends StatefulWidget {
+  final Widget? child;
+  final GetConfigCallback getConfigCallback;
+
+  const _StatefulConfigWidget(
+    this.getConfigCallback, {
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  _StatefulConfigWidgetState createState() => _StatefulConfigWidgetState();
+}
+
+class _StatefulConfigWidgetState extends State<_StatefulConfigWidget>
+    with NavBarConfigMixin {
+  @override
+  Widget build(BuildContext context) => widget.child ?? const Center();
+
+  @override
+  NavBarConfig get navBarConfig => widget.getConfigCallback();
+}
