59cf65de921875f7d9f276dec64e7fd896793b6d
Andrii Antonov <pro100svitlo@gmail.com>
2022-01-10 09:03:40 +0100

    Add all colors from new palette

--- lib/src/linden/xcolors.dart
+++ lib/src/linden/xcolors.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 @immutable
 class XColors {
   final Brightness _brightness;
+
   const XColors({Brightness? brightness})
       : _brightness = brightness ?? Brightness.light;
 
@@ -37,12 +38,19 @@ class XColors {
   // for your use case.
   // Try to keep naming as general as possible
   Color get primary => _theme(bright: _blue, dark: _white);
+
   Color get accent => _theme(bright: _green);
+
   Color get secondaryAccent => _theme(bright: _orange);
+
   Color get primaryText => _theme(bright: _black, dark: _lightGrey);
+
   Color get secondaryText => _theme(bright: _darkGrey);
+
   Color get tertiaryText => _theme(bright: _mediumGrey);
+
   Color get tooltipText => _theme(bright: _blue, dark: _white);
+
   @Deprecated(
       'should be replaced with `iconNew`. Be aware about the bright color change there')
   Color get icon => _theme(bright: _black, dark: _white);
@@ -50,102 +58,168 @@ class XColors {
   //ignore:deprecated_member_use_from_same_package
   /// later, when [icon] color will be removed, the name will be changed to [icon]
   Color get iconNew => _theme(bright: _blue, dark: _white);
+
   Color get iconDisabled => _theme(bright: _mediumGrey, dark: _darkGrey);
+
   Color get iconSelected => _theme(bright: _green);
+
   Color get iconInverse => _theme(bright: _white);
+
   Color get iconBackground => _theme(bright: _lightGrey, dark: _black);
+
   Color get iconBackgroundSelected => _theme(bright: _white, dark: _white);
+
   Color get secondaryIcon => _theme(bright: _blue);
 
   Color get primaryTextInverse => _theme(bright: _white, dark: _black);
+
   Color get pageBackground => _theme(bright: _white, dark: _black29);
+
   Color get cardBackground => _theme(bright: _white, dark: _black33);
+
   Color get background => _theme(bright: _white, dark: _black);
+
   Color get videoBackground => _theme(bright: _black);
+
   Color get tooltipBackground => _theme(bright: _white, dark: _black);
+
   Color get hintTextStyle => _theme(bright: _darkGrey, dark: _mediumGrey);
 
   Color get imageBackground => _theme(bright: _white);
+
   Color get imageBackgroundOverlay => imageBackground.withAlpha(200);
+
   Color get imageDetailsBackground => _theme(bright: _trueBlack).withAlpha(120);
+
   Color get imagePreviewBackground => _theme(bright: _trueBlack);
 
   Color get secondaryActionText => _theme(bright: _mediumGrey);
+
   Color get divider => _theme(bright: _mediumGrey);
+
   Color get shadow => _theme(bright: _darkGrey);
+
   Color get searchSuggetsionShadow => _theme(bright: _black.withAlpha(20));
+
   Color get searchSuggestion => _theme(bright: _lightGreen);
+
   Color get shadowTransparent =>
       _theme(bright: _black.withAlpha(65), dark: _black.withAlpha(0));
+
   Color get shadowTransparentDarker =>
       _theme(bright: _black.withAlpha(100), dark: _black.withAlpha(0));
+
   Color get transparent => _transparent;
+
   Color get searchInputFill => _theme(bright: _lightGrey, dark: _black33);
+
   Color get searchInputText => _theme(bright: _black, dark: _lightGrey);
+
   Color get searchInputTextDisabled =>
       _theme(bright: _mediumGrey, dark: _darkGrey);
+
   Color get searchPuller => _theme(bright: _mediumGrey);
+
   Color get searchResultSkeletonBase => _theme(bright: _mediumGrey);
+
   Color get searchResultSkeletonHighlight => _theme(bright: _lightGrey);
+
   Color get cardTitle => _theme(bright: _black, dark: _lightGrey);
+
   Color get cardCaption => _theme(bright: _mediumGrey);
+
   Color get cardBorderHighlight => _theme(bright: _green);
+
   Color get imagePlaceholderBackground => _theme(bright: _mediumGrey);
+
   Color get swipeBackgroundRelevant => _theme(bright: _green);
+
   Color get swipeBackgroundNeutral => _theme(bright: _mediumGrey);
+
   Color get swipeBackgroundBookmark => _theme(bright: _purple);
+
   Color get swipeBackgroundMoveBookmark => _theme(bright: _purple);
+
   Color get swipeBackgroundEdit => _theme(bright: _purple);
+
   Color get swipeBackgroundShare => _theme(bright: _white);
+
   Color get swipeBackgroundIrrelevant => _theme(bright: _pink);
+
   Color get swipeBackgroundDelete => _theme(bright: _pink);
+
   Color get overlayBackground => _theme(bright: _black.withAlpha(96));
+
   Color get searchInputBarShadowBackground =>
       _theme(bright: primary.withAlpha(10));
+
   Color get snackbarBackground => _theme(bright: _blue.withAlpha(227));
+
   Color get placeholderIcon => _theme(bright: _mediumGrey);
+
   Color get autosuggestionMarketChange => _theme(bright: _purple);
+
   Color get imageBlurColor => _theme(bright: _white.withOpacity(0.333));
 
   Color? get logo => _themeDarkModeColorOrNull(dark: _white);
 
   /// Colors that are independent of the dark mode
   Color get onboardingBackground => _white;
+
   Color get onboardingTutorialBackground => _black.withOpacity(0.85);
+
   Color get onboardingTextSelected => _green;
+
   Color get onboardingTextInverse => _white;
+
   Color get onboardingTagTextInverse => _white;
+
   Color get onboardingSelectedTagBackground => _purple;
+
   Color get onboardingTagBackground => _white;
+
   Color get onboardingSearchInputFill => _lightGrey;
+
   Color get checkboxUnchecked => _lightGrey;
+
   Color get checkboxChecked => _green;
 
   Color get speedDialText => _black;
+
   Color get brightText => _white;
+
   Color get brightIcon => _white;
+
   Color get darkIcon => _blue;
+
   Color get primaryAction => _pink;
+
   Color get inputErrorText => _pink;
 
   Color get webViewBottomBarActionBackground => _purple;
 
   Color get speedDialButtonSelected => _pink;
+
   Color get speedDialButtonUnSelected => _lightGrey;
 
   Color get qrCodeBackground => _white;
 
   Color get swipeCardBackground => _black;
+
   Color get snippetBackground => _white;
+
   Color get snippetTextColor => _black;
 
   /// Dots indicator
   Color get dotsIndicatorColor => _transparent;
+
   Color get dotsIndicatorActiveColor => _white;
+
   Color get dotsIndicatorBorderColor => _white;
 
   /// Color helper constants
   final alpha50 = 0x7f;
+
   Color _theme({
     required Color bright,
     Color? dark,
@@ -165,15 +239,49 @@ class XColors {
   /// So far I added here some items, which I need for my tasks.
   /// But later we will restructure all this file with proper items (names and structure)
   //region NEW COLORS
-  static const _newPrimary = Color(0xff009278);
 
+  // New Primary
+  static const _newPrimary = Color(0xff009278);
+  static const _newPrimary20 = Color(0xffCCE6E2);
+  static const _newPrimary40 = Color(0xff99CEC5);
+  static const _newPrimary60 = Color(0xff66B5A7);
+  static const _newPrimary80 = Color(0xff339D8A);
+  static const _newPrimary120 = Color(0xff006B58);
+
+  /// New Grey Scale
   static const _newWhite = Color(0xffFFFFFF);
   static const _newBlack = Color(0xff0F1A18);
   static const _newBlack5 = Color(0xffF8F8F8);
+  static const _newBlack10 = Color(0xffE2E4E4);
   static const _newBlack20 = Color(0xffCACDCD);
+  static const _newBlack40 = Color(0xff9BA0A0);
   static const _newBlack60 = Color(0xff6D7372);
   static const _newBlack80 = Color(0xff3E4645);
   static const _newBlack95 = Color(0xff1B2524);
+
+  /// New Accent Pink
+  static const _newPink = Color(0xffDB1870);
+  static const _newPink20 = Color(0xffF3CDDE);
+  static const _newPink40 = Color(0xffEDA0C3);
+  static const _newPink60 = Color(0xffE772A7);
+  static const _newPink80 = Color(0xffE1458B);
+  static const _newPink120 = Color(0xffC21563);
+
+  /// New Accent Purple
+  static const _newPurple = Color(0xff544998);
+  static const _newPurple20 = Color(0xffDDDBEA);
+  static const _newPurple40 = Color(0xffBBB6D6);
+  static const _newPurple60 = Color(0xff9892C1);
+  static const _newPurple80 = Color(0xff766DAD);
+  static const _newPurple120 = Color(0xff463D80);
+
+  /// New Accent Orange
+  static const _newOrange = Color(0xffF09005);
+  static const _newOrange20 = Color(0xffFCE9CD);
+  static const _newOrange40 = Color(0xffF9D39B);
+  static const _newOrange60 = Color(0xffF6BC69);
+  static const _newOrange80 = Color(0xffF3A637);
+  static const _newOrange120 = Color(0xffD68004);
 
   Color get newPrimary => _theme(bright: _newPrimary);
 
