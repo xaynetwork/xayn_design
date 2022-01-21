1838b6d6756e020a410614d28e23639d5da2df5b
Michael <michael.k.malak@gmail.com>
2022-01-21 11:22:00 +0200

    use useRootNavigator for bottom sheet

--- lib/src/widget/bottom_sheet/bottom_sheet_body_mixin.dart
+++ lib/src/widget/bottom_sheet/bottom_sheet_body_mixin.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:xayn_design/src/widget/nav_bar/widget/nav_bar_container.dart';
 
 mixin BottomSheetBodyMixin {
   ScrollController? getScrollController(BuildContext context) =>
       ModalScrollController.of(context);
 
-  void closeBottomSheet(
-    BuildContext context, {
-    bool showNavBar = true,
-  }) {
+  void closeBottomSheet(BuildContext context) {
     Navigator.pop(context);
-    onCloseBottomSheet(context, showNavBar: showNavBar);
-  }
-
-  void onCloseBottomSheet(
-    BuildContext context, {
-    bool showNavBar = true,
-  }) {
-    if (showNavBar) {
-      NavBarContainer.showNavBar(context);
-    }
   }
 }
--- lib/src/widget/bottom_sheet/show_app_bottom_sheet.dart
+++ lib/src/widget/bottom_sheet/show_app_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:xayn_design/src/widget/nav_bar/widget/nav_bar_container.dart';
 import 'package:xayn_design/src/widget/unter_den_linden/unter_den_linden.dart';
 
 import 'bottom_sheet_base.dart';
@@ -11,16 +10,13 @@ Future showAppBottomSheet(
   BuildContext context, {
   required _BottomSheetBuilder builder,
   bool showBarrierColor = true,
-  bool hideNavBar = true,
 }) {
-  if (hideNavBar) {
-    NavBarContainer.hideNavBar(context);
-  }
   final linden = UnterDenLinden.getLinden(context);
 
   return showMaterialModalBottomSheet(
     context: context,
     enableDrag: false,
+    useRootNavigator: true,
     elevation: linden.dimen.elevationHigh,
     shape: RoundedRectangleBorder(
       borderRadius: linden.styles.roundBorderBottomSheet,
